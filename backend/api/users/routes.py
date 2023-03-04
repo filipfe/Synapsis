723d29ade0460b18b9d38b0ee914dda104283936
Sebastian <se6359@gmail.com>
2023-03-04 15:09:21 +0100

    admin

--- backend/api/users/routes.py
+++ backend/api/users/routes.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 
 from db.database import get_db
+from security.auth import get_current_user, oauth2_scheme
 
 from . import schemas, crud
 
@@ -10,7 +11,10 @@ router = APIRouter()
 
 
 @router.post("/signup")
-async def signup(user: schemas.Signup, db: Session = Depends(get_db)):
+async def signup(user: schemas.Signup, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    user = get_current_user(db, token)
+    if not user.is_admin:
+        raise HTTPException(status_code=400, detail="No permision")
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already exists")
