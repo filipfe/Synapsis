f92d807c65f7902ea31013bf298378e0bdbb9649
Filip <filfer05@gmail.com>
2023-03-04 15:38:28 +0100

    route fix

--- frontend/src/consts/routes.tsx
+++ frontend/src/consts/routes.tsx
@@ -3,19 +3,19 @@ import Occurance from "../pages/autism/Occurance";
 import Symptoms from "../pages/autism/Symptoms";
 
 export const autismRoutes = [
-    {
-        path: '/definicja',
-        title: 'DEFINICJA AUTYZMU',
-        component: <Definition />
-    },
-    {
-        path: '/objawy',
-        title: 'OBJAWY AUTYZMU',
-        component: <Symptoms />
-    },
-    {
-        path: '/występowanie',
-        title: 'WYSTĘPOWANIE AUTYZMU',
-        component: <Occurance />
-    }
-]
+  {
+    path: "/definicja",
+    title: "DEFINICJA AUTYZMU",
+    component: <Definition />,
+  },
+  {
+    path: "/objawy",
+    title: "OBJAWY AUTYZMU",
+    component: <Symptoms />,
+  },
+  {
+    path: "/wystepowanie",
+    title: "WYSTĘPOWANIE AUTYZMU",
+    component: <Occurance />,
+  },
+];
